2b6ac01422b4eac7f527d516ee78843e7dd0bb87
mo khan <mo@mokhan.ca>
2017-01-28 10:10:29 -0700

    add return 404 when in maintenance mode.

--- app/controllers/bloodpressure/health_controller.rb
+++ app/controllers/bloodpressure/health_controller.rb
@@ -4,6 +4,17 @@ module Bloodpressure
   class HealthController < ApplicationController
     def show
       @hostname = Socket.gethostname
+      render status: http_status
+    end
+
+    private
+
+    def http_status
+      File.readable?(maintenance_file) ? :not_found : :ok
+    end
+
+    def maintenance_file
+      Rails.root.join('tmp', 'maintenance')
     end
   end
 end
--- test/controllers/bloodpressure/health_controller_test.rb
+++ test/controllers/bloodpressure/health_controller_test.rb
@@ -4,9 +4,25 @@ module Bloodpressure
   class HealthControllerTest < ActionDispatch::IntegrationTest
     include Engine.routes.url_helpers
 
+    setup do
+      @maintenance_file = Rails.root.join('tmp', 'maintenance')
+    end
+
+    teardown do
+      File.unlink(@maintenance_file) if File.readable?(@maintenance_file)
+    end
+
     test "returns the hostname" do
       get health_url
       assert_response :success
+      assert_select "h1", Socket.gethostname
+    end
+
+    test "returns 404, when in maintenance mode" do
+      FileUtils.touch(@maintenance_file)
+
+      get health_url
+      assert_response :not_found
       assert_select "h1", Socket.gethostname
     end
   end
